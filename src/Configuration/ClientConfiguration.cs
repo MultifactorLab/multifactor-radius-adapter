03cd7e455b39482ba11e52fdd245150bd50e738c
Aleksey Pashkov <56426436+apashkov-ext@users.noreply.github.com>
2023-03-30 17:46:25 +0600

    partial mode was added (#17)

--- src/Configuration/ClientConfiguration.cs
+++ src/Configuration/ClientConfiguration.cs
@@ -63,7 +63,7 @@ namespace MultiFactor.Radius.Adapter.Configuration
         /// </summary>
         public bool BypassSecondFactorWhenApiUnreachable { get; set; }
 
-        public PrivacyMode PrivacyMode { get; set; }
+        public PrivacyModeDescriptor PrivacyMode { get; set; }
 
 
         #region ActiveDirectory Authentication settings
