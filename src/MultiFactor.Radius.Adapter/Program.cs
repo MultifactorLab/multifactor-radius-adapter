0fab02b0d22c7d7f3ae084c5aa9692e2477badbe
Oleg Brezhnev <oleg-423@yandex.ru>
2024-03-26 17:27:31 +0300

    Enable log to console on a startup-failed error

--- src/MultiFactor.Radius.Adapter/Program.cs
+++ src/MultiFactor.Radius.Adapter/Program.cs
@@ -2,23 +2,24 @@
 using Microsoft.Extensions.Hosting;
 using MultiFactor.Radius.Adapter;
 using Serilog;
+using Serilog.Events;
 using System;
 using System.Text;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.ConfigureApplication();
-
-var host = builder.Build();
-
+IHost host = null;
 try
 {
+    var builder = Host.CreateApplicationBuilder(args);
+    builder.ConfigureApplication();
+
+    host = builder.Build();
     host.Run();
 }
 catch (Exception ex)
 {
     var errorMessage = FlattenException(ex);
 
-    if (Log.Logger != null)
+    if (Log.Logger != null && Log.IsEnabled(LogEventLevel.Error))
     {
         Log.Logger.Error($"Unable to start: {errorMessage}");
     }
