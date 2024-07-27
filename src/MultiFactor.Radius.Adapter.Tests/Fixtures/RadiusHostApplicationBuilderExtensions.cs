ffd484b0a016be97844b9799b908b9c91392c13f
Aleksey Pashkov <pashkov.a.n@yandex.ru>
2024-07-27 12:36:12 +0500

    test factories were changed

---
+++ src/MultiFactor.Radius.Adapter.Tests/Fixtures/RadiusHostApplicationBuilderExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Moq;
+using MultiFactor.Radius.Adapter.Core.Framework;
+using MultiFactor.Radius.Adapter.Core.Framework.Pipeline;
+using MultiFactor.Radius.Adapter.Infrastructure.Configuration;
+using MultiFactor.Radius.Adapter.Infrastructure.Configuration.ConfigurationLoading;
+using MultiFactor.Radius.Adapter.Infrastructure.Configuration.Core;
+using MultiFactor.Radius.Adapter.Tests.Fixtures.ConfigLoading;
+
+namespace MultiFactor.Radius.Adapter.Tests.Fixtures;
+
+internal static class RadiusHostApplicationBuilderExtensions
+{
+    public static RadiusHostApplicationBuilder WithTestConfiguration(this RadiusHostApplicationBuilder builder, 
+        Action<TestConfigProviderOptions>? configure = null)
+    {
+        builder.Services.AddOptions<TestConfigProviderOptions>();
+        if (configure is not null)
+        {
+            builder.Services.Configure(configure);
+        }
+        
+        builder.Services.ReplaceService(prov =>
+        {
+            var opt = prov.GetRequiredService<IOptions<TestConfigProviderOptions>>().Value;
+            var rootConfig = TestRootConfigProvider.GetRootConfiguration(opt);
+            var factory = prov.GetRequiredService<ServiceConfigurationFactory>();
+
+            var config = factory.CreateConfig(rootConfig);
+            config.Validate();
+
+            return config;
+        });
+
+        builder.Services.ReplaceService<IClientConfigurationsProvider, TestClientConfigsProvider>();
+        
+        return builder;
+    }
+
+    public static RadiusHostApplicationBuilder MockUdp(this RadiusHostApplicationBuilder builder)
+    {
+        builder.Services.ReplaceService<Func<IPEndPoint, IUdpClient>>(prov => endpoint => new Mock<IUdpClient>().Object);
+        return builder;
+    }
+    
+    public static RadiusHostApplicationBuilder MockRadiusRequestPostProcessor(this RadiusHostApplicationBuilder builder)
+    {
+        builder.Services.ReplaceService(new Mock<IRadiusRequestPostProcessor>().Object);
+        return builder;
+    }
+}
--- src/MultiFactor.Radius.Adapter.Tests/Fixtures/TestHostFactory.cs
+++ src/MultiFactor.Radius.Adapter.Tests/Fixtures/TestHostFactory.cs
@@ -1,49 +1,25 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
-using Microsoft.Extensions.Options;
-using Moq;
 using MultiFactor.Radius.Adapter.Core.Framework;
-using MultiFactor.Radius.Adapter.Core.Framework.Pipeline;
 using MultiFactor.Radius.Adapter.Extensions;
-using MultiFactor.Radius.Adapter.Infrastructure.Configuration;
-using MultiFactor.Radius.Adapter.Infrastructure.Configuration.ConfigurationLoading;
-using MultiFactor.Radius.Adapter.Infrastructure.Configuration.Core;
-using MultiFactor.Radius.Adapter.Tests.Fixtures.ConfigLoading;
-using System.Net;
 
 namespace MultiFactor.Radius.Adapter.Tests.Fixtures;
 
 internal static class TestHostFactory
 {
     /// <summary>
-    /// Creates a generic Radius Test Host.
+    /// Creates a generic Radius Test Host with "Test" value of ASPNETCORE_ENVIRONMENT variable.
     /// </summary>
     /// <param name="configure">Configure host action.</param>
     /// <returns></returns>
     public static TestHost CreateHost(Action<RadiusHostApplicationBuilder>? configure = null)
     {
-        var builder = RadiusHost.CreateApplicationBuilder(new[] { "--environment", "Test" });
+        var builder = CreateRadiusHostBuilder();
 
-        builder.Services.ReplaceService<Func<IPEndPoint, IUdpClient>>(prov => endpoint => new Mock<IUdpClient>().Object);
-
-        builder.Services.AddOptions<TestConfigProviderOptions>();
-        builder.Services.ReplaceService(prov =>
-        {
-            var opt = prov.GetRequiredService<IOptions<TestConfigProviderOptions>>().Value;
-            var rootConfig = TestRootConfigProvider.GetRootConfiguration(opt);
-            var factory = prov.GetRequiredService<ServiceConfigurationFactory>();
-
-            var config = factory.CreateConfig(rootConfig);
-            config.Validate();
-
-            return config;
-        });
-
-        builder.Services.ReplaceService<IClientConfigurationsProvider, TestClientConfigsProvider>();
-
-        builder.Services.ReplaceService(new Mock<IRadiusRequestPostProcessor>().Object);
-
+        builder.MockUdp();
+        builder.WithTestConfiguration();
+        builder.MockRadiusRequestPostProcessor();
+        
         builder.Services.ReplaceService<ILoggerFactory, NullLoggerFactory>();
 
         builder.ConfigureApplication();
@@ -52,5 +28,14 @@ internal static class TestHostFactory
         var host = builder.Build();
         var testHost = new TestHost(host);
         return testHost;
+    }
+
+    /// <summary>
+    /// Creates a generic Radius Host with "Test" value of ASPNETCORE_ENVIRONMENT variable.
+    /// </summary>
+    /// <returns></returns>
+    public static RadiusHostApplicationBuilder CreateRadiusHostBuilder()
+    {
+        return RadiusHost.CreateApplicationBuilder(new[] { "--environment", "Test" });
     }
 }
