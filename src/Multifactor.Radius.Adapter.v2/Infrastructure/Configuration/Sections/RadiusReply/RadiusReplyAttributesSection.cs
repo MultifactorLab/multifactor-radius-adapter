1d78b4ed08841f1f280a87410cdc8cb4ae3fb324
s.naidenov <tussss89@gmail.com>
2025-04-21 16:54:32 +0700

    DEV-662

---
+++ src/Multifactor.Radius.Adapter.v2/Infrastructure/Configuration/Sections/RadiusReply/RadiusReplyAttributesSection.cs
@@ -0,0 +1,38 @@
+﻿//Copyright(c) 2020 MultiFactor
+//Please see licence at 
+//https://github.com/MultifactorLab/multifactor-radius-adapter/blob/main/LICENSE.md
+
+using System.ComponentModel;
+using Microsoft.Extensions.Configuration;
+
+namespace Multifactor.Radius.Adapter.v2.Infrastructure.Configuration.Sections.RadiusReply;
+
+[Description("Attributes")]
+public class RadiusReplyAttributesSection
+{
+    [ConfigurationKeyName("add")]
+    private RadiusReplyAttribute[] _elements { get; set; }
+
+    [ConfigurationKeyName("add")]
+    private RadiusReplyAttribute _singleElement { get; set; }
+
+    public RadiusReplyAttribute[] Elements
+    {
+        get
+        {
+            // To deal with a single element binding to array issue, we should map a single claim manually 
+            // See: https://github.com/dotnet/runtime/issues/57325
+            if (!string.IsNullOrWhiteSpace(_singleElement?.Name))
+            {
+                return new [] { _singleElement };
+            }
+        
+            if (_elements != null && _elements.All(x => !string.IsNullOrWhiteSpace(x.Name)))
+            {
+                return _elements;
+            }
+
+            return Array.Empty<RadiusReplyAttribute>();
+        }
+    }
+}
