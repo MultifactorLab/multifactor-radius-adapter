e713eac772fbabb9ae9e511f0102d0c37cb427f7
s.naidenov <tussss89@gmail.com>
2025-06-23 11:48:54 +0700

    DEV-681

--- src/Multifactor.Radius.Adapter.v2.Tests/PipelineTests/StepsTests/SecondFactorStepTests.cs
+++ src/Multifactor.Radius.Adapter.v2.Tests/PipelineTests/StepsTests/SecondFactorStepTests.cs
@@ -1,6 +1,8 @@
 using System.Net;
 using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
+using Multifactor.Core.Ldap.Name;
+using Multifactor.Core.Ldap.Schema;
 using Multifactor.Radius.Adapter.v2.Core.AccessChallenge;
 using Multifactor.Radius.Adapter.v2.Core.Auth;
 using Multifactor.Radius.Adapter.v2.Core.Configuration.Client;
@@ -16,6 +18,18 @@ namespace Multifactor.Radius.Adapter.v2.Tests.PipelineTests.StepsTests;
 
 public class SecondFactorStepTests
 {
+    [Fact]
+    public void EmptyContext_ShouldThrowArgumentNullException()
+    {
+        var apiServiceMock = new Mock<IMultifactorApiService>();
+        var challengeProviderMock = new Mock<IChallengeProcessorProvider>();
+        var groupServiceMock = new Mock<ILdapGroupService>();
+        
+        var step = new SecondFactorStep(apiServiceMock.Object, challengeProviderMock.Object, groupServiceMock.Object, NullLogger<SecondFactorStep>.Instance);
+        
+        Assert.ThrowsAsync<ArgumentNullException>(() => step.ExecuteAsync(null));
+    }
+
     [Fact]
     public async Task ExecuteAsync_AclRequest_ShouldSetBypass()
     {
@@ -37,7 +51,7 @@ public class SecondFactorStepTests
         var ldapConfig = new Mock<ILdapServerConfiguration>();
         ldapConfig.Setup(x => x.SecondFaGroups).Returns(new List<string>());
         ldapConfig.Setup(x => x.SecondFaBypassGroups).Returns(new List<string>());
-        contextMock.Setup(x => x.FirstFactorLdapServerConfiguration).Returns(ldapConfig.Object); 
+        contextMock.Setup(x => x.Settings.LdapServerConfiguration).Returns(ldapConfig.Object); 
         
         var context = contextMock.Object;
         context.AuthenticationState = new AuthenticationState();
@@ -51,7 +65,7 @@ public class SecondFactorStepTests
     [InlineData(AuthenticationStatus.Reject)]
     [InlineData(AuthenticationStatus.Accept)]
     [InlineData(AuthenticationStatus.Awaiting)]
-    public async Task ExecuteAsync_ShouldSecondFactorStatus(AuthenticationStatus apiResponseStatus)
+    public async Task ExecuteAsync_NoBypass_ShouldSecondFactorStatus(AuthenticationStatus apiResponseStatus)
     {
         var apiServiceMock = new Mock<IMultifactorApiService>();
         apiServiceMock.Setup(x => x.CreateSecondFactorRequestAsync(It.IsAny<IRadiusPipelineExecutionContext>())).ReturnsAsync(new MultifactorResponse(apiResponseStatus, "state", "message"));
@@ -76,7 +90,7 @@ public class SecondFactorStepTests
         var ldapConfig = new Mock<ILdapServerConfiguration>();
         ldapConfig.Setup(x => x.SecondFaGroups).Returns(new List<string>());
         ldapConfig.Setup(x => x.SecondFaBypassGroups).Returns(new List<string>());
-        contextMock.Setup(x => x.FirstFactorLdapServerConfiguration).Returns(ldapConfig.Object); 
+        contextMock.Setup(x => x.Settings.LdapServerConfiguration).Returns(ldapConfig.Object); 
         
         var context = contextMock.Object;
         context.AuthenticationState = new AuthenticationState();
@@ -117,7 +131,7 @@ public class SecondFactorStepTests
         var ldapConfig = new Mock<ILdapServerConfiguration>();
         ldapConfig.Setup(x => x.SecondFaGroups).Returns(new List<string>());
         ldapConfig.Setup(x => x.SecondFaBypassGroups).Returns(new List<string>());
-        contextMock.Setup(x => x.FirstFactorLdapServerConfiguration).Returns(ldapConfig.Object); 
+        contextMock.Setup(x => x.Settings.LdapServerConfiguration).Returns(ldapConfig.Object); 
         
         var context = contextMock.Object;
         context.AuthenticationState = new AuthenticationState();
@@ -128,5 +142,77 @@ public class SecondFactorStepTests
         Assert.Equal("state", context.ResponseInformation.State);
         Assert.Equal("message", context.ResponseInformation.ReplyMessage);
         processorMock.Verify(x => x.AddChallengeContext(context), Times.Once);
+    }
+
+    [Fact]
+    public async Task ExecuteAsync_MemberOf2FaBypassGroups_ShouldBypass()
+    {
+        var apiServiceMock = new Mock<IMultifactorApiService>();
+        var challengeProviderMock = new Mock<IChallengeProcessorProvider>();
+        var groupServiceMock = new Mock<ILdapGroupService>();
+        groupServiceMock.Setup(x => x.IsMemberOf(It.IsAny<MembershipRequest>())).Returns(true);
+        var step = new SecondFactorStep(apiServiceMock.Object, challengeProviderMock.Object, groupServiceMock.Object, NullLogger<SecondFactorStep>.Instance);
+        
+        var ldapConfig = new Mock<ILdapServerConfiguration>();
+        ldapConfig.Setup(x => x.SecondFaGroups).Returns(new List<string>());
+        ldapConfig.Setup(x => x.SecondFaBypassGroups).Returns(new List<string>() { "dc=bypass, dc=group" });
+        ldapConfig.Setup(x => x.LoadNestedGroups).Returns(false);
+        ldapConfig.Setup(x => x.NestedGroupsBaseDns).Returns([]);
+        ldapConfig.Setup(x => x.ConnectionString).Returns("string");
+        ldapConfig.Setup(x => x.UserName).Returns("username");
+        ldapConfig.Setup(x => x.Password).Returns("password");
+        ldapConfig.Setup(x => x.BindTimeoutInSeconds).Returns(30);
+        
+        var contextMock = new Mock<IRadiusPipelineExecutionContext>();
+        contextMock.Setup(x => x.Settings.LdapServerConfiguration).Returns(ldapConfig.Object);
+        contextMock.Setup(x => x.UserLdapProfile.Dn).Returns(new DistinguishedName("dc=bypass,dc=group,dc=member"));
+        contextMock.Setup(x => x.UserLdapProfile.MemberOf).Returns([]);
+        contextMock.Setup(x => x.RequestPacket.UserName).Returns("userName");
+        contextMock.Setup(x => x.LdapSchema).Returns(new Mock<ILdapSchema>().Object);
+        contextMock.SetupProperty(x => x.AuthenticationState);
+        
+        var context = contextMock.Object;
+        context.AuthenticationState = new AuthenticationState();
+        context.ResponseInformation = new ResponseInformation();
+        
+        await step.ExecuteAsync(context);
+        
+        Assert.Equal(AuthenticationStatus.Bypass, context.AuthenticationState.SecondFactorStatus);
+    }
+    
+    [Fact]
+    public async Task ExecuteAsync_NotMemberOf2FaGroups_ShouldBypass()
+    {
+        var apiServiceMock = new Mock<IMultifactorApiService>();
+        var challengeProviderMock = new Mock<IChallengeProcessorProvider>();
+        var groupServiceMock = new Mock<ILdapGroupService>();
+        groupServiceMock.Setup(x => x.IsMemberOf(It.IsAny<MembershipRequest>())).Returns(false);
+        var step = new SecondFactorStep(apiServiceMock.Object, challengeProviderMock.Object, groupServiceMock.Object, NullLogger<SecondFactorStep>.Instance);
+        
+        var ldapConfig = new Mock<ILdapServerConfiguration>();
+        ldapConfig.Setup(x => x.SecondFaGroups).Returns(new List<string>() { "dc=bypass, dc=group" });
+        ldapConfig.Setup(x => x.SecondFaBypassGroups).Returns(new List<string>());
+        ldapConfig.Setup(x => x.LoadNestedGroups).Returns(false);
+        ldapConfig.Setup(x => x.NestedGroupsBaseDns).Returns([]);
+        ldapConfig.Setup(x => x.ConnectionString).Returns("string");
+        ldapConfig.Setup(x => x.UserName).Returns("username");
+        ldapConfig.Setup(x => x.Password).Returns("password");
+        ldapConfig.Setup(x => x.BindTimeoutInSeconds).Returns(30);
+        
+        var contextMock = new Mock<IRadiusPipelineExecutionContext>();
+        contextMock.Setup(x => x.Settings.LdapServerConfiguration).Returns(ldapConfig.Object);
+        contextMock.Setup(x => x.UserLdapProfile.Dn).Returns(new DistinguishedName("dc=bypass,dc=group,dc=member"));
+        contextMock.Setup(x => x.UserLdapProfile.MemberOf).Returns([]);
+        contextMock.Setup(x => x.LdapSchema).Returns(new Mock<ILdapSchema>().Object);
+        contextMock.Setup(x => x.RequestPacket.UserName).Returns("userName");
+        contextMock.SetupProperty(x => x.AuthenticationState);
+        
+        var context = contextMock.Object;
+        context.AuthenticationState = new AuthenticationState();
+        context.ResponseInformation = new ResponseInformation();
+        
+        await step.ExecuteAsync(context);
+        
+        Assert.Equal(AuthenticationStatus.Bypass, context.AuthenticationState.SecondFactorStatus);
     }
 }
--- src/Multifactor.Radius.Adapter.v2/Infrastructure/Pipeline/Steps/SecondFactorStep.cs
+++ src/Multifactor.Radius.Adapter.v2/Infrastructure/Pipeline/Steps/SecondFactorStep.cs
@@ -1,7 +1,7 @@
 using Microsoft.Extensions.Logging;
+using Multifactor.Core.Ldap.Name;
 using Multifactor.Radius.Adapter.v2.Core.AccessChallenge;
 using Multifactor.Radius.Adapter.v2.Core.Auth;
-using Multifactor.Radius.Adapter.v2.Core.Configuration.Client;
 using Multifactor.Radius.Adapter.v2.Core.MultifactorApi;
 using Multifactor.Radius.Adapter.v2.Infrastructure.Pipeline.Context;
 using Multifactor.Radius.Adapter.v2.Services.Ldap;
@@ -25,39 +25,74 @@ public class SecondFactorStep : IRadiusPipelineStep
 
     public async Task ExecuteAsync(IRadiusPipelineExecutionContext context)
     {
-        if (!ShouldCallSecondFactor(context.FirstFactorLdapServerConfiguration))
-            return;
+        ArgumentNullException.ThrowIfNull(context);
         
-        if (context.RequestPacket.IsVendorAclRequest)
+        if (!ShouldCallSecondFactor(context))
         {
-            // security check
-            if (context.Settings.FirstFactorAuthenticationSource == AuthenticationSource.Radius)
-            {
-                _logger.LogInformation("Bypass second factor for user '{user:l}' from {host:l}:{port}",
-                    context.RequestPacket.UserName,
-                    context.RemoteEndpoint.Address,
-                    context.RemoteEndpoint.Port);
-
-                context.AuthenticationState.SecondFactorStatus = AuthenticationStatus.Bypass;
-                await Task.CompletedTask;
-                return;
-            }
+            context.AuthenticationState.SecondFactorStatus = AuthenticationStatus.Bypass;
+            await Task.CompletedTask;
+            return;
         }
 
         var apiResponse = await _multifactorApiService.CreateSecondFactorRequestAsync(context);
         ProcessApiResponse(context, apiResponse);
     }
 
-    //TODO add 2fa group check
-    private bool ShouldCallSecondFactor(ILdapServerConfiguration configuration)
+    private bool ShouldCallSecondFactor(IRadiusPipelineExecutionContext context)
     {
-        if (configuration.SecondFaGroups.Any())
-            return true;
-
-        if (configuration.SecondFaBypassGroups.Any())
+        if (ShouldBypassByRequest(context))
+        {
+            _logger.LogInformation("Second factor is bypassed for user '{user:l}' from {host:l}:{port}",
+                context.RequestPacket.UserName,
+                context.RemoteEndpoint.Address,
+                context.RemoteEndpoint.Port);
             return false;
+        }
+
+        if (ShouldBypassByGroups(context))
+        {
+            _logger.LogInformation("Second factor is bypassed for user {user:l} in '{domain:l}'", context.RequestPacket.UserName, context.Settings.LdapServerConfiguration.ConnectionString);
+            return false;
+        }
         
         return true;
+    }
+
+    private bool ShouldBypassByRequest(IRadiusPipelineExecutionContext context)
+    {
+        return context.RequestPacket.IsVendorAclRequest && context.Settings.FirstFactorAuthenticationSource == AuthenticationSource.Radius;
+    }
+
+    private bool ShouldBypassByGroups(IRadiusPipelineExecutionContext context)
+    {
+        var serverConfig = context.Settings.LdapServerConfiguration;
+        bool? bypassMember = null;
+
+        if (serverConfig.SecondFaBypassGroups.Any())
+        {
+            var request = GetMembershipRequest(context, serverConfig.SecondFaBypassGroups);
+            bypassMember = _ldapGroupService.IsMemberOf(request);
+        }
+
+        if (bypassMember is true)
+        {
+            _logger.LogInformation("User '{user:l}' is a member of the 2FA bypass group in '{domain:l}'", context.RequestPacket.UserName, serverConfig.ConnectionString);
+            return true;
+        }
+
+        bool? secondFactorMember = null;
+        if (serverConfig.SecondFaGroups.Any())
+        {
+           var request = GetMembershipRequest(context, serverConfig.SecondFaGroups);
+           secondFactorMember = _ldapGroupService.IsMemberOf(request);
+           if (secondFactorMember is false)
+               _logger.LogInformation("User '{user:l}' is not a member of the 2FA group in '{domain:l}'", context.RequestPacket.UserName, serverConfig.ConnectionString);
+        }
+
+        if (secondFactorMember.HasValue)
+            return !secondFactorMember.Value;
+        
+        return false;
     }
 
     private void ProcessApiResponse(IRadiusPipelineExecutionContext context, MultifactorResponse apiResponse)
@@ -74,5 +109,23 @@ public class SecondFactorStep : IRadiusPipelineStep
                 throw new InvalidOperationException("Challenge processor could not be found");
             challengeProcessor.AddChallengeContext(context);
         }
+    }
+
+    private MembershipRequest GetMembershipRequest(IRadiusPipelineExecutionContext context, IEnumerable<string> targetGroupsNames)
+    {
+        var groupDns = targetGroupsNames.Select(x => new DistinguishedName(x)).ToArray();
+        
+        return new MembershipRequest(
+            context.UserLdapProfile.Dn,
+            context.UserLdapProfile.MemberOf.ToArray(),
+            context.Settings.LdapServerConfiguration.LoadNestedGroups,
+            context.Settings.LdapServerConfiguration.NestedGroupsBaseDns.Select(x => new DistinguishedName(x)).ToArray(),
+            context.Settings.LdapServerConfiguration.ConnectionString,
+            context.Settings.LdapServerConfiguration.UserName,
+            context.Settings.LdapServerConfiguration.Password,
+            context.Settings.LdapServerConfiguration.BindTimeoutInSeconds,
+            context.LdapSchema!,
+            groupDns
+        );
     }
 }
