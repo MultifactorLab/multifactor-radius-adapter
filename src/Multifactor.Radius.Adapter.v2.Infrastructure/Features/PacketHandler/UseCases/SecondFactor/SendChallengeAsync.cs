ffbb45f8452ef7df9b132f98d202cf295a401585
aleksey.trofimov <alexey.trofimov@multifactor.ru>
2026-07-01 13:29:50 +0300

    DEV-1539 log API requests and response bodies in SendChallenge

--- src/Multifactor.Radius.Adapter.v2.Infrastructure/Features/PacketHandler/UseCases/SecondFactor/SendChallengeAsync.cs
+++ src/Multifactor.Radius.Adapter.v2.Infrastructure/Features/PacketHandler/UseCases/SecondFactor/SendChallengeAsync.cs
@@ -1,6 +1,6 @@
 using System.Net;
 using System.Net.Http.Headers;
-using System.Net.Http.Json;
+using System.Text;
 using System.Text.Json;
 using Microsoft.Extensions.Logging;
 using Multifactor.Radius.Adapter.v2.Application.Features.PacketHandler.UseCases.SecondFactor.Multifactor.Exceptions;
@@ -31,11 +31,15 @@ internal sealed class SendChallenge: ISendChallenge
     {
         ArgumentNullException.ThrowIfNull(dto, nameof(dto));
         using var client = CreateClient(authData);
+        
+        var requestJson = JsonSerializer.Serialize(dto, _jsonOptions);
+        _logger.LogDebug("Sending request to API: {Request:l}", requestJson);
+
         try
         {
-            var response = await client.PostAsJsonAsync(Url,
-                dto, _jsonOptions, cancellationToken);
-            
+            using var requestContent = new StringContent(requestJson, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(Url, requestContent, cancellationToken);
+        
             return await ProcessResponseAsync(response, cancellationToken);
         }
         catch (HttpRequestException ex)
@@ -81,6 +85,9 @@ internal sealed class SendChallenge: ISendChallenge
         }
         
         var content = await response.Content.ReadAsStringAsync(cancellationToken);
+        
+        _logger.LogDebug("Received response from API: {Response:l}", content);
+        
         var apiResponse = JsonSerializer.Deserialize<MultiFactorApiResponse<AccessRequestResponse>>(
             content, 
             _jsonOptions);
